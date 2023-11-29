56ccf318ff9512a06d468eeb43e9824c16c1d6b3
Aman Kumar Singh <79279299+Jr-Einstein@users.noreply.github.com>
2023-11-29 18:15:33 +0530

    Intro to Datatypes of JS

---
+++ index.js
@@ -0,0 +1,28 @@
+//Datatypes in JS ( Fast Forward)
+
+
+var name = "Aman Kumar Singh";
+console.log(name);
+
+var int  = "34";
+console.log(int);
+
+var age = "16";
+console.log(age);
+
+//array
+var arr=[19,23,20,18,20];
+console.log(arr[0]);
+
+var user1=["aman","aniket","anjan","sehej"];
+console.log(user1);
+
+//entering name and age in same array
+
+var name_and_age=["aman",16,"harki",20,"sehej",23,"Anjan",13];
+console.log(name_and_age);
+
+//better way to write is 
+
+var usernamesage=[name="Aman",age="18"]
+console.log(usernamesage);
