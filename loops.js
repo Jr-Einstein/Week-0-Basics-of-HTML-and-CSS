2046646e7a1e7a19b409baccb9b420ae4166f8aa
Aman Kumar Singh <79279299+Jr-Einstein@users.noreply.github.com>
2023-11-29 18:57:55 +0530

    Loops

---
+++ loops.js
@@ -0,0 +1,7 @@
+//Printing the users using for loop
+
+var users = [{ name: "Aman", age: 18 }, { name: "harkirat", age: 27 }, { name: "Yash Pandey", age: 19 }, { name: "Nirmal Anand", age: 20 }];
+
+for (var i = 0; i < users[i].length + 1; i++) {
+  console.log(users[i].name + "'s age is " + users[i].age);
+}
